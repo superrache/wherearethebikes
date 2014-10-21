6fe9d270644e4b68707c8c24974b98b51113aeeb
superrache <superrache@hotmail.fr>
2014-10-21 17:43:28 +0200

    adding stations

--- src/wherearethebikes/WherearethebikesServlet.java
+++ src/wherearethebikes/WherearethebikesServlet.java
@@ -1,11 +1,65 @@
 package wherearethebikes;
+import java.io.BufferedReader;
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 
-@SuppressWarnings("serial")
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class WherearethebikesServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	public static final String urlBase = "https://api.jcdecaux.com/vls/v1/";
+	public static final String apiKey = "446183c9a7001ea4a0c71060d15c26603c623f49";
+	public static final String encoding = "UTF-8";
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setCharacterEncoding(encoding);
+		
+		String q = req.getParameter("q");
+		try {
+			URL stationsRequestUrl = new URL(urlBase + "stations?contract=" + URLEncoder.encode(q, encoding) + "&apiKey=" + apiKey);
+			
+			HttpURLConnection connection = (HttpURLConnection)stationsRequestUrl.openConnection();
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.setRequestMethod("GET");
+			connection.setReadTimeout(10000);
+			connection.setConnectTimeout(15000);
+			connection.connect();
+			InputStream stationsInputStream = connection.getInputStream();
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(stationsInputStream, encoding));
+			StringBuilder responseStrBuilder = new StringBuilder();
+
+		    String inputStr;
+		    while ((inputStr = streamReader.readLine()) != null) {
+		    	responseStrBuilder.append(inputStr);
+		    }
+		    
+		    //JSONObject stationsJSONObject = new JSONObject(responseStrBuilder.toString());
+			
+			resp.getWriter().println(responseStrBuilder.toString());
+			
+		} catch(MalformedURLException e) {
+			resp.getWriter().println("{\"error\":\"0\"}");
+		} catch(ProtocolException e) {
+			resp.getWriter().println("{\"error\":\"0\"}");
+		} catch(IOException e) {
+			resp.getWriter().println("{\"error\":\"0\"}");
+		} catch(NullPointerException e) {
+			resp.getWriter().println("{\"error\":\"0\"}");
+		} catch(Exception e) {
+			resp.getWriter().println("{\"error\":\"0\"}");
+		}	
+		
 	}
+	
 }
--- war/index.js
+++ war/index.js
@@ -1,44 +1,89 @@
 var map = L.map('map').setView([45.762858, 4.855000], 13);
         mapLink = 
             '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18,
-            }).addTo(map);
+        L.tileLayer('http://{s}.tiles.mapbox.com/v3/kevinlecocq.jfk00chc/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 20
+        	}).addTo(map);
 				
 	/* Initialize the SVG layer */
-	map._initPathRoot()    
+	map._initPathRoot();
  
 	/* We simply pick up the SVG from the map object */
 	var svg = d3.select("#map").select("svg"),
 	g = svg.append("g");
 	
-	d3.json("data.json", function(collection) {
+	d3.json("wherearethebikes?q=Lyon", function(stations) {
 		/* Add a LatLng object to each item in the dataset */
-		collection.objects.forEach(function(d) {
-			d.LatLng = new L.LatLng(d.station.coordinates[0],
-									d.station.coordinates[1])
+		stations.forEach(function(d) {
+			d.LatLng = new L.LatLng(d.position.lat, d.position.lng);
 		})
 		
-		var feature = g.selectAll("circle")
-			.data(collection.objects)
+		/*var vertices = d3.range(stations.length).map(function(d, i) {
+			return [stations[i].LatLng.lat, stations[i].LatLng.lng];
+		});*/
+		
+		var voronoi = d3.geom.voronoi()
+			.x(function(d) {
+					return d.LatLng[0]; 
+				})
+			.y(function(d) { 
+					return d.LatLng[1]; 
+				});
+		
+		voronoi(stations).forEach(function(d) { d.point.cell = d; });
+		
+		//var voronoiGraph = d3.geom.voronoi(vertices);
+		
+		var stationCircles = g.selectAll("circle")
+			.data(stations)
 			.enter().append("circle")
 			.style("stroke", "black")  
 			.style("opacity", .6) 
-			.style("fill", "red")
-			.attr("r", 20);  
+			.style("fill", function(d, i) {
+				if(d.available_bikes) return "green";
+				else return "red";
+				})
+			.attr("r", 3);
+		
+		/*var stationPaths = g.selectAll("path")
+			.data(voronoi)
+			.enter().append("svg:path")
+			.attr("class", function(d, i) {
+					return i ? "q" + (i % 9) + "-9" : null; 
+				})
+			.attr("d", function(d) { 
+					return "M" + d.join("L") + "Z";
+					//return "M" + d.source.LatLng.lat
+				});*/
+		var stationPaths = g.selectAll("path")
+			.data(stations)
+			.enter().append("svg:path")
+			.attr("class", function(d, i) {
+					return i ? "q" + (i % 9) + "-9" : null; 
+				})
+			.attr("d", function(d) { 
+					return "M" + d.join("L") + "Z";
+				});
 		
 		map.on("viewreset", update);
 		update();
- 
+		
 		function update() {
-			feature.attr("transform", 
+			stationCircles.attr("transform", 
 			function(d) { 
 				return "translate("+ 
 					map.latLngToLayerPoint(d.LatLng).x +","+ 
 					map.latLngToLayerPoint(d.LatLng).y +")";
 				}
 			)
+			
+			stationPaths.attr("transform",
+					function(d) {
+						return "translate(" + 
+							map.latLngToLayerPoint(new L.LatLng(d[0], d[1])).x +","+ 
+							map.latLngToLayerPoint(new L.LatLng(d[0], d[1])).y +")";
+					}
+				)
 		}
 	})
