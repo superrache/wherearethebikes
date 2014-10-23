9841d4996e9262e701e5fba578ee1d29d486e127
superrache <superrache@hotmail.fr>
2014-10-23 17:20:25 +0200

    Update index.js
    diagram generation for each map update

--- war/index.js
+++ war/index.js
@@ -1,89 +1,106 @@
 var map = L.map('map').setView([45.762858, 4.855000], 13);
-        mapLink = 
-            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer('http://{s}.tiles.mapbox.com/v3/kevinlecocq.jfk00chc/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 20
-        	}).addTo(map);
+mapLink = 
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	minZoom:1,
+    maxZoom: 19
+}).addTo(map);
 				
-	/* Initialize the SVG layer */
+// Initialize the SVG layer
 	map._initPathRoot();
- 
-	/* We simply pick up the SVG from the map object */
-	var svg = d3.select("#map").select("svg"),
-	g = svg.append("g");
+
+	// svg selection
+var svg = d3.select("#map").select("svg"),
+	stationAreaGroup = svg.append("g"),
+	stationPointGroup = svg.append("g");
+
+d3.json("wherearethebikes?q=Lyon", function(stations) {
 	
-	d3.json("wherearethebikes?q=Lyon", function(stations) {
-		/* Add a LatLng object to each item in the dataset */
-		stations.forEach(function(d) {
-			d.LatLng = new L.LatLng(d.position.lat, d.position.lng);
-		})
-		
-		/*var vertices = d3.range(stations.length).map(function(d, i) {
-			return [stations[i].LatLng.lat, stations[i].LatLng.lng];
-		});*/
-		
-		var voronoi = d3.geom.voronoi()
-			.x(function(d) {
-					return d.LatLng[0]; 
-				})
-			.y(function(d) { 
-					return d.LatLng[1]; 
-				});
-		
-		voronoi(stations).forEach(function(d) { d.point.cell = d; });
-		
-		//var voronoiGraph = d3.geom.voronoi(vertices);
-		
-		var stationCircles = g.selectAll("circle")
-			.data(stations)
-			.enter().append("circle")
-			.style("stroke", "black")  
-			.style("opacity", .6) 
-			.style("fill", function(d, i) {
-				if(d.available_bikes) return "green";
-				else return "red";
-				})
-			.attr("r", 3);
-		
-		/*var stationPaths = g.selectAll("path")
-			.data(voronoi)
-			.enter().append("svg:path")
-			.attr("class", function(d, i) {
-					return i ? "q" + (i % 9) + "-9" : null; 
-				})
-			.attr("d", function(d) { 
-					return "M" + d.join("L") + "Z";
-					//return "M" + d.source.LatLng.lat
-				});*/
-		var stationPaths = g.selectAll("path")
-			.data(stations)
-			.enter().append("svg:path")
-			.attr("class", function(d, i) {
-					return i ? "q" + (i % 9) + "-9" : null; 
-				})
-			.attr("d", function(d) { 
-					return "M" + d.join("L") + "Z";
-				});
-		
-		map.on("viewreset", update);
-		update();
-		
-		function update() {
-			stationCircles.attr("transform", 
-			function(d) { 
-				return "translate("+ 
-					map.latLngToLayerPoint(d.LatLng).x +","+ 
-					map.latLngToLayerPoint(d.LatLng).y +")";
-				}
-			)
-			
-			stationPaths.attr("transform",
-					function(d) {
-						return "translate(" + 
-							map.latLngToLayerPoint(new L.LatLng(d[0], d[1])).x +","+ 
-							map.latLngToLayerPoint(new L.LatLng(d[0], d[1])).y +")";
-					}
-				)
-		}
+	stations.forEach(function(d) {
+		d.LatLng = new L.LatLng(d.position.lat, d.position.lng);
 	})
+	
+	var stationPoints = stationPointGroup.selectAll("circle")
+		.data(stations)
+		.enter().append("circle")
+		.style("stroke", "black")  
+		.style("opacity", .6) 
+		.style("fill", function(d, i) {
+			if(d.available_bikes) return "green";
+			else return "red";
+			})
+		.attr("r", 2);
+	
+	var colorScale = d3.scale.linear()
+							.range(["red", "yellow", "green"])
+							.domain([0, 0.5, 1]);
+	
+	var stationAreas = stationAreaGroup.selectAll("path")
+		.data(stations)
+		.enter().append("svg:polygon")
+		.attr("id", function(d, i) {
+				return "station-" + i;
+			})
+		.attr("stroke-width", "0px")
+		.attr("fill-opacity", "0.5")
+		.attr("fill", function(d, i) {
+				var bikeAvailability = d.available_bikes / d.bike_stands;
+				var standAvailability = d.available_bike_stands / d.bike_stands;
+				return colorScale(standAvailability);
+			});
+	
+	map.on("viewreset", update);
+	update();
+	
+	function update() {
+		// Data reprojection
+		stations.forEach(function(d, i) {
+				var stationPoint = map.latLngToLayerPoint(d.LatLng);
+				d.x = stationPoint.x;
+				d.y = stationPoint.y;
+			});
+		
+		//var pixelBounds = map.getPixelBounds();
+		
+		// Voronoi computation, clipped by the new map frame
+		var voronoi = d3.geom.voronoi()
+		.x(function(d) {
+				return d.x; 
+			})
+		.y(function(d) { 
+				return d.y; 
+			});
+		//.clipExtent([[pixelBounds.min.x, pixelBounds.min.y], [pixelBounds.max.x, pixelBounds.max.y]]);
+	
+		voronoi(stations).forEach(function(d) { 
+				d.point.cell = d;
+			});
+	
+		stationPoints.attr("transform", function(d) { 
+			return "translate("+ d.x +","+ d.y +")";
+			}
+		);
+		
+		stationAreas.attr("points", function(d) {
+			var polygon = "";
+			var first = true;
+			for(var i = 0; i < d.cell.length; i++) {
+				if(isNaN(d.cell[i][0]) || isNaN(d.cell[i][1])) return "";
+				
+				//var latLngPoint = new L.LatLng(d.cell[i][0], d.cell[i][1]);
+				//var screenPoint = map.latLngToLayerPoint(latLngPoint);
+				if(first) {
+					first = false;
+				} else {
+					polygon += " ";
+				} 
+				//polygon += screenPoint.x + "," + screenPoint.y;
+				
+				polygon += d.cell[i][0] + "," + d.cell[i][1];
+			}
+			return polygon;
+		});
+
+	}
+});
